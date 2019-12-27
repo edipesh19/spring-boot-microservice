a9635770515befc9652225b8f08aa6984d7afdc9
Dipesh Dutta <dipesh.dutta@oracle.com>
2019-12-27 21:24:50 +0530

    Implement limit and config serv

---
+++ limit-serv/src/main/java/org/sb/microservice/limitserv/LimitServ.java
@@ -0,0 +1,11 @@
+package org.sb.microservice.limitserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitServ {
+    public static void main(String[] args) {
+        SpringApplication.run(LimitServ.class, args);
+    }
+}
